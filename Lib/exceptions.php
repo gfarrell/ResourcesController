fcec5c748fe0466a4ff7e9ae31224d799b62ee98
Gideon Farrell <me@gideonfarrell.co.uk>
2012-06-13 14:16:39 +0100

    Created some custom exceptions

---
+++ Lib/exceptions.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+	Plugin Exceptions 
+	-----------------
+
+	@file 		exceptions.php
+	@author 	Gideon Farrell <me@gideonfarrell.co.uk>
+
+	Copyright (c) 2012 Gideon Farrell <http://www.gideonfarrell.co.uk>
+
+*/
+
+class MissingResourceConfigurationException extends CakeException {
+	protected $_messageTemplate = 'It seems that the configuration file for the ResourcesController plugin is missing. Please create ResourcesController.xml in the Config directory';
+}
+
+class MissingResourcePackageException extends CakeException {
+	protected $_messageTemplate = 'It seems that the package %s cannot be found.';
+}
+
+class MissingResourceFileException extends CakeException {
+	protected $_messageTemplate = 'It seems that the file %s (in package %s) cannot be found.';
+}
