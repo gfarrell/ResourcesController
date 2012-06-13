4838a03a1f98074cd9017f1b059c05af0dc598a1
Gideon Farrell <me@gideonfarrell.co.uk>
2012-06-13 13:11:03 +0100

    Created a preprocessor interface

---
+++ Lib/iPreprocessor.php
@@ -0,0 +1,35 @@
+<?php
+/*
+
+	Preprocessor Interface
+	----------------------
+
+	@file 		iPreprocessor.php
+	@author 	Gideon Farrell <me@gideonfarrell.co.uk>
+
+	Copyright (c) 2012 Gideon Farrell <http://www.gideonfarrell.co.uk>
+
+*/
+
+interface iPreprocessor {
+	/**
+	 * process
+	 * Processes the script.
+	 * 
+	 * @access public
+	 * @param string $script_in the script to be processed
+	 * @param bool $compress whether or not to perform compression
+	 * @return string processed script
+	 */
+	public function process($script_in, $compress=false);
+
+	/**
+	 * compress
+	 * Compresses the script.
+	 * 
+	 * @access public
+	 * @param string $script_in the script to be compressed
+	 * @return string compressed script
+	 */
+	public function compress($script_in); 
+}
