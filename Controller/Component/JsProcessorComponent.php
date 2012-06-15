552b87b261a96631622f5d3bc856182d025b4bf8
Gideon Farrell <me@gideonfarrell.co.uk>
2012-06-15 15:33:31 +0100

    Made JsProcessorComponent use proper JavaScriptPacker (I got the name wrong originally) and made it use the correctly named interface as well

--- Controller/Component/JsProcessorComponent.php
+++ Controller/Component/JsProcessorComponent.php
@@ -11,19 +11,19 @@
 
 */
 
-App::uses('iProcessor', 'ResourcesController.Lib');
-App::import('ResourcesController.Vendor', 'JavascriptPacker', array('file' => 'JavascriptPacker' . DS . 'class.JavascriptPacker.php'));
+App::uses('iPreprocessor', 'ResourcesController.Lib');
+App::import('ResourcesController.Vendor', 'JavaScriptPacker/JavaScriptPacker');
 
-class JsProcessor extends Component implements iProcessor {
+class JsProcessorComponent extends Component implements iPreprocessor {
 	public function process($script_in, $compress=false) {
 		return $compress ? $this->compress($script_in) : $script_in;
 	}
 
 	public function compress($script_in) {
 		try {
-			$packer = new JavascriptPacker($script_in);
+			$packer = new JavaScriptPacker($script_in);
 		} catch(Exception $e) {
-			throw new PreprocessFailureException(array('processor'=>'JavascriptPacker'));
+			throw new PreprocessFailureException(array('processor'=>'JavaScriptPacker'));
 		}
 		return $packer->pack();
 	}
